da1fd92489bd4c5c5958dc99a86fb8d71d15f8d5
Luana <ltdc01@hotmail.com>
2021-08-20 22:50:18 -0300

    Movimentacao do barco e configurações iniciais

---
+++ WGJ2021/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    private float horizontalMovement, verticalMovement;
+    private float speed = 5f;
+
+    private float energy = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move(){
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+        
+        rigidBody.velocity = new Vector2(horizontalMovement*speed, verticalMovement*speed);
+    }
+}
